860fa218dfaa0c6ea0fdc6880395941100a22a13
MURAD-CBRG <murad.efendiev05@yandex.ru>
2021-04-20 16:59:23 +0300

    MainCommit

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'INFORMATION_SECURITY'
 
 
-@app.route('/carousel')
+@app.route('/carousel', methods=['GET', 'POST'])
 def carousel_func():
-    params = {
-        'title': 'ИНФОРМАЦИОННАЯ БЕЗОПАСНОСТЬ'
-    }
+    if request.method == 'GET':
+        params = {
+            'title': 'ИНФОРМАЦИОННАЯ БЕЗОПАСНОСТЬ',
+            'data': list(filter(lambda x: x != '1.png', os.listdir('static/img')))
+        }
 
-    return render_template('info_sec.html', params=params)
+        return render_template('info_sec.html', params=params)
+    elif request.method == 'POST':
+        file = request.files['file']
+
+        new_file = open('static/img/img{0}.jpg'.format(len(os.listdir('static/img')) + 1), mode='wb')
+        new_file.write(file.read())
+        new_file.close()
+
+        return "Форма отправлена"
+
 
 
 def main():
